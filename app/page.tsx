3919d8b6292bbdc046e3b8c0660049646d930810
shayanmahmood <shayanmahmood234@gmail.com>
2025-05-14 10:24:14 -0700

    first

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Client";
+import Experience from "@/components/Experiences";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import Hero from "@/components/Hero";
+import RecentProjects from "@/components/RecentProjects";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
